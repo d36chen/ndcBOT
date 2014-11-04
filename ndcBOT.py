e541f85576e3f6a00a5bc1fc26ffa5f1052a5563
Chen <dannchen@amazon.com>
2014-11-03 19:03:32 -0800

    Added getting links

--- ndcBOT.py
+++ ndcBOT.py
@@ -1,5 +1,6 @@
 import mechanize
 import cookielib
+import re
 
 # Browser
 br = mechanize.Browser()
@@ -8,7 +9,19 @@ br = mechanize.Browser()
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 
-r = br.open('http://www.nike.com')
-html = r.read()
+r = br.open('http://store.nike.com/us/en_us/pw/mens-shoes/7puZbrk')
+#html = r.read()
 
-print html
+p = re.compile('lebron.*12', re.IGNORECASE)
+
+for link in br.links():
+	m1 = p.search(link.url)
+	m2 = p.search(link.url)
+	if m1 or m2:
+    		print link.text, link.url
+
+# Actually clicking the link
+#req = br.click_link(text='Weekend codes')
+#br.open(req)
+#print br.response().read()
+#print br.geturl()
